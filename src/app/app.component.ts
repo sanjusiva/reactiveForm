873dec12d73250ea8a56c10e7b579a3c999fb9d7
sanjusiva <sanjusiru123@gmail.com>
2022-05-11 18:21:54 +0530

    reactiveForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup,FormControl,FormControlName,Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForms';
+  newForm=new FormGroup({
+    fname:new FormControl('',[Validators.required]),
+    lname:new FormControl(''),
+    mobNo:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+  })
+  userForm(){
+    console.warn(this.newForm.value);
+    
+  }
+  get fname(){
+    return this.newForm.get('fname');
+  }
+  get mobNo(){
+    return this.newForm.get('mobNo');
+  }
+  get email(){
+    return this.newForm.get('email');
+  }
 }
